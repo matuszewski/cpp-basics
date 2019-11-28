0861e9eaa0480370fea06dac84fed2b67d74a0cc
matuszewski <33099194+matuszewski@users.noreply.github.com>
2019-11-28 09:55:11 +0100

    Update getline.cpp

--- getline.cpp
+++ getline.cpp
@@ -6,8 +6,15 @@ int main() {
   char * napis = new char[128];
   
   cout<<"Napiz cos: ";
-  cin.getline(napis, 128);
+  cin.getline(napis, 128);  //zmienna i dlugosc przchwytywanych danych
   cout<< napis<<endl;
+  
+  ofstream zapis;
+  zapis.open("plik.txt", ofstream::app);  //append czyli dopisywnaie nie nadpisywanie
+  zapis<<napis<<endl;
+  
+  zapis.close();
+  delete [] napis;
   
   return 0;
 }
