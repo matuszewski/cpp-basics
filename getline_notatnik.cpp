0b010576645a66229b754b131f60b8260dacb741
matuszewski <33099194+matuszewski@users.noreply.github.com>
2019-11-28 10:26:02 +0100

    Update getline_notatnik.cpp

--- getline_notatnik.cpp
+++ getline_notatnik.cpp
@@ -1,39 +1,41 @@
 #include <iostream>
-
+#include <fstream>
 using namespace std;
 
 int main() {
   char * napis = new char[128];
-  
+
   cout<<"Napiz cos: ";
   cin.getline(napis, 128);  //zmienna i dlugosc przchwytywanych danych
   cout<< napis<<endl;
-  
+    char d[] = "";
+  cin>>d;
+
   ofstream zapis;
-  zapis.open("plik.txt", ofstream::app);  //append czyli dopisywnaie nie nadpisywanie
+  zapis.open(d, ofstream::app);  //append czyli dopisywnaie nie nadpisywanie
   zapis<<napis<<endl;
-  
+
   zapis.close();
   delete [] napis;
-  
-  
+
+
   char * wczytane = new char[128];
   ifstream odczyt;
   odczyt.open("plik.txt");
-  
+
   if(odczyt.is_open()) {
     while(!odczyt.getline(wczytane, 128)) {
       cout<<odczyt;
     }
     odczyt.close();
     cout<<"Zakonczono czytanie pliku"<<endl;
-    
+
   } else {
     cout<<"Nie udalo sie otworzyc pliku / plik nie istnieje"<<endl;
   }
   delete[] wczytane;
-  
+
   // ^^ to wszystko mozna zrobic poprzez while(!odczyt.eof())
- 
+
   return 0;
 }
